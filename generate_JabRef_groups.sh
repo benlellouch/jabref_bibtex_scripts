e3f6a36e56a53f9e360c4b48206edac67c921bc9
Benjamin Lellouch <43215208+benlellouch@users.noreply.github.com>
2020-03-18 18:51:01 +0000

    added scripts that updates groupings based on groups present in articles meta-data

---
+++ generate_JabRef_groups.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#removes previous groupings blocks
+sed -r 'H;1h;$!d;x; s/@Comment\{jabref-meta: grouping:.*}//g' $1 > temp
+
+#opens new groupings block and adds parent entry
+echo -e "@Comment{jabref-meta: grouping:\n0 AllEntriesGroup:;" >> temp
+
+#scans through groups in articles and mics and creates an entry for every group
+grep -E "groups(\s)*=" temp | sed -r 's/(groups(\s)*=\s\{)|(\},)//g' | sed -r 's/(\s)*//g' |sed -r 's/,/\n/g' | sort -u | while read line
+do
+	echo "1 StaticGroup:$line\;0\;1\;\;\;\;;" >> temp
+done
+
+#closes the block
+echo "}" >> temp
+
+# renames temp file to file given as argument
+mv temp $1
