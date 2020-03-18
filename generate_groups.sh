ba3d81555ae43c249bef01df05b6f87c7c1f4767
Benjamin Lellouch <blellouch@gmail.com>
2020-03-18 19:01:45 +0000

    reduced sed calls into one

--- generate_groups.sh
+++ generate_groups.sh
@@ -7,7 +7,7 @@ sed -r 'H;1h;$!d;x; s/@Comment\{jabref-meta: grouping:.*}//g' $1 > temp
 echo -e "@Comment{jabref-meta: grouping:\n0 AllEntriesGroup:;" >> temp
 
 #scans through groups in articles and mics and creates an entry for every group
-grep -E "groups(\s)*=" temp | sed -r 's/(groups(\s)*=\s\{)|(\},)//g' | sed -r 's/(\s)*//g' |sed -r 's/,/\n/g' | sort -u | while read line
+grep -E "groups(\s)*=" temp | sed -r 's/(groups(\s)*=\s\{)|(\},)//g; s/(\s)*//g; s/,/\n/g' | sort -u | while read line
 do
 	echo "1 StaticGroup:$line\;0\;1\;\;\;\;;" >> temp
 done
