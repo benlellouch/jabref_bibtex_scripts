c5ee2bc025b079d4220874ad9836a9a32efeddf3
Benjamin Lellouch <43215208+benlellouch@users.noreply.github.com>
2021-03-26 23:45:47 +0000

    Update generate_groups.sh

--- generate_groups.sh
+++ generate_groups.sh
@@ -6,7 +6,7 @@ sed -r 'H;1h;$!d;x; s/@Comment\{jabref-meta: grouping:.*}//g' $1 > temp
 #opens new groupings block and adds parent entry
 echo -e "@Comment{jabref-meta: grouping:\n0 AllEntriesGroup:;" >> temp
 
-#scans through groups in articles and mics and creates an entry for every group
+#scans through groups in articles and miscs and creates an entry for every group
 grep -E "groups(\s)*=" temp | sed -r 's/(groups(\s)*=\s\{)|(\},)//g; s/(\s)*//g; s/,/\n/g' | sort -u | while read line
 do
 	echo "1 StaticGroup:$line\;0\;1\;\;\;\;;" >> temp
